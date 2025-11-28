5b5e73479ea4a4b1c5b925f12ed9e33b54c40735
abhishek kumar <abhishek.ak.vii@gmail.com>
2025-11-29 01:53:09 +0530

    configure cloudflared

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tunnel from "astro-tunnel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tunnel({
+      // // The URL to expose. Defaults to http://localhost:4324.
+      url: "http://localhost:4324",
+      // // The local server port to expose. Defaults to 4324. Only used if `url` is not set.
+      // port: 4324,
+      // // The local server protocol to use. Defaults to http. Only used if `url` is not set.
+      // protocol: "http",
+      // // Whether to verify the local server TLS certificate. Defaults to false.
+      // verifyTLS: false,
+      // // Whether to accept the Cloudflare terms of service. Defaults to false.
+      // acceptCloudflareNotice: false,
+    }),
+  ],
+});
